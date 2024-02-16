71d578b14e333dc48ae89ff384fc7ae42732f1b4
geosen <43538334+geosen@users.noreply.github.com>
2024-02-16 14:00:25 +0200

    Add files via upload

--- R/wgcna_enrich_modules.R
+++ R/wgcna_enrich_modules.R
@@ -1,20 +1,29 @@
-#' Perform enrichment analysis on WGCNA gene lists using the latest version of gprofiler2
+#' Perform enrichment analysis on WGCNA gene lists using the latest version of clusterProfiler
 #' 
 #' 
 #' 
 #'Dependencies are:
-#' library(gprofiler2)
+#' library(clusterProfiler)
 #' library(openxlsx)
+#' library(dplyr)
+#' library(msigdbr)
 #'
 #'date 02/08/2023
 #'@param wgcna_gene_info Table containing gene names and the module they belong
-#'@param gene_id_cols The name of the column containing gene ids
-#'@param organism The organism to search for: hsapiens or mmusculus
+#'@param gene_id_cols The name of the column containing gene ids (HGNC_Symbols)
+#'@param organism The organism to search for: Homo sapiens or Mus musculus
 #'@param modules A vector containing the modules to be tested. If this is given, any other module parameter is overriden. 
 #'@param module_Results A data frame with module names as rownames containing columns with correlation p.values between module eigengenes and phenotypical traits.
 #'@param significant_only TRUE/FALSE. Whether to check only modules that are significantly correlated with at least 1 phenotypical trait or check every module detected.
 #'@param export_to_xlsx TRUE/FALSE. Whether to export results for each module to an excel file.
-#'
+#'@param genesets The collections of MSigDB R package to check enrichment of in the format of Category;Subcategory.
+#'@param fdr_threshold The FDR threshold for results exported in Excel file
+#'@param pvalueCutoff ClusterProfiler's adjusted pvalue cutoff on enrichment tests to report
+#'@param pAdjustMethod ClusterProfiler's: one of "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none"
+#'@param export_fdr_thres Used when export to xlsx is TRUE to filter the final list of geneset results to be exported. By default, it equals the pvalueCutoff.
+#'@param minGSSize ClusterProfiler's minimal size of genes annotated for testing
+#'@param maxGSSize ClusterProfiler's maximal size of genes annotated for testing
+#'@param qvalueCutoff qvalue cutoff on enrichment tests to report as significant. Tests must pass i) pvalueCutoff on unadjusted pvalues, ii) pvalueCutoff on adjusted pvalues and iii) qvalueCutoff on qvalues to be reported.
 #'
 #'@return A list with enrichment results as elements.
 #'@export
@@ -26,19 +35,24 @@
 #'
 
 wgcna_enrich_modules <- function(wgcna_gene_info,
-                                 gene_id_cols = 'EnsemblGeneID',
+                                 gene_id_cols = 'geneSymbol',
                                  module_color_column = 'moduleColor',
-                                 organism = 'hsapiens',
+                                 organism = 'Homo sapiens',
                                  modules = NULL,
                                  module_Results = NULL,
                                  significant_only = TRUE,
-                                 export_to_xlsx = FALSE) {
+                                 export_to_xlsx = FALSE,
+                                 collections = c('H','C2;CP','C5;GO:BP'),
+                                 pvalueCutoff = 0.05,
+                                 export_fdr_thres = 0.05,
+                                 pAdjustMethod = 'BH',
+                                 minGSSize = 10,
+                                 maxGSSize = 500,
+                                 qvalueCutoff = 1) {
   
-  if(!requireNamespace("gprofiler2")) {
-    stop('Please load gprofiler2 to continue')
+  if(!requireNamespace("clusterProfiler")) {
+    stop('Please load clusterProfiler to continue')
   }
-  
-  gost_res <- list()
   
   if(significant_only == TRUE & is.null(modules)) {
     stopifnot('To check for significance you have to provide module_Results data frame' =  !is.null(module_Results))
@@ -66,21 +80,68 @@ wgcna_enrich_modules <- function(wgcna_gene_info,
     wb <- createWorkbook()
   }
   
+  ##Defining genesets 
+  categories <- as.data.frame(stringr::str_split(collections,';',n = 2,simplify = TRUE))
   
+  #Checking categories
+  stopifnot('Invalid category entered. Please check spelling!' = all(pull(categories,1) %in% unique(pull(msigdbr_collections(),'gs_cat'))))
+  #checking subcategories
+  stopifnot('Invalid subcategory entered. Please check spelling!' = all(pull(categories,2) %in% unique(pull(msigdbr_collections(),'gs_subcat'))))
   
-  ##enrichment check loop
+  #Gathering genesets
+  for (i in 1:dim(categories)[1]) {
+    
+    if(i == 1) {
+      genesets <- msigdbr(species = organism,
+                          category= categories[i,1],
+                          subcategory = categories[i,2])
+    } else {
+      genesets <- genesets %>%
+        bind_rows(msigdbr(species = organism,
+                          category= categories[i,1],
+                          subcategory = categories[i,2]))
+    }
+    
+  }
+  
+  ##creating background
+  background <- geneInfo %>%
+    filter(!duplicated(geneSymbol),!is.na(geneSymbol), geneSymbol != '') %>%
+    pull(geneSymbol)
+    
+  
+  ##Creating Term2Gene
+  t2g_cp <- genesets %>%
+    dplyr::distinct(gs_name,
+                    gene_symbol) %>%
+    as.data.frame()
+  
+    #creating list for storing results
+    cp_res <- list()
+  
+    ##enrichment check loop
   for (i in 1:length(modules)) {
     #selecting module
     module <- modules[i]
     #isolating gene list
     genes <- geneInfo %>%
       filter(get(module_color_column) == module) %>%
-      mutate(ensg = remove_version(get(gene_id_cols))) %>%
-      dplyr::select(ensg, paste0('MM.',module), paste0('p.MM.',module))
-    #running enrichment
-    gostMod <- gost(genes$ensg, organism = organism)
+      mutate(geneSymb = lightsabeR::remove_version(get(gene_id_cols))) %>%
+      dplyr::select(geneSymb, paste0('MM.',module), paste0('p.MM.',module))
     
-    gost_res[[module]] <- gostMod
+    
+    #running enrichment
+    cpMod <- enricher(genes$geneSymb,
+                        pvalueCutoff = pvalueCutoff,
+                        pAdjustMethod = pAdjustMethod,
+                        universe = background,
+                        minGSSize = minGSSize,
+                        maxGSSize = maxGSSize,
+                        qvalueCutoff = qvalueCutoff,
+                        TERM2GENE = t2g_cp)
+    
+    
+    cp_res[[module]] <- cpMod
     
     if(export_to_xlsx) {
       
@@ -90,7 +151,8 @@ wgcna_enrich_modules <- function(wgcna_gene_info,
       
       addWorksheet(wb = wb, sheetName = module)
       
-      writeData(wb = wb, sheet = module, x = gostMod$result)
+      writeDataTable(wb = wb, sheet = module, x = cpMod@result %>%
+                       filter(p.adjust < export_fdr_thres))
                  
     }
     
@@ -99,14 +161,14 @@ wgcna_enrich_modules <- function(wgcna_gene_info,
   
   if(export_to_xlsx){
     saveWorkbook(wb, 
-                 paste0('gprofiler_enrichment_',
+                 paste0('clusterProfiler_enrichment_',
                         gsub('\\.[[:digit:]]+','',
                              as.numeric(Sys.time())),
                         '.xlsx')
                  )
     
   }
-  gost_res
+  cp_res
   
   
 }
