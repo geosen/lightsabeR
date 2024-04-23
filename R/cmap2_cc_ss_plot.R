bdcc11a1346ce2dd291a996e68472700ecc99ccc
geosen <43538334+geosen@users.noreply.github.com>
2024-04-23 11:19:41 +0300

    Add files via upload

--- R/cmap2_cc_ss_plot.R
+++ R/cmap2_cc_ss_plot.R
@@ -4,7 +4,8 @@
 #' library(cmapR)
 #' library(tidyverse)
 #' library(ggrepel)
-#'
+#' 
+#' 
 #'date 22/04/2024
 #'@param cmap_melted A melted data.frame containing Connectivity Map 2 results
 #'@param n_top_highlight Number of top results to label
@@ -14,6 +15,8 @@
 #'@param min.segment.length geom_label_repel minimum segment length parameter
 #'@param label_alpha Transparency parameter for labels
 #'@param label_size Size parameter for labels
+#'@param tas_color Color of the tas lines
+#'@param tas_linetype Linetype of the tas lines
 #'
 #'
 #'
@@ -24,7 +27,9 @@ cmap2_cc_ss_plot <- function(cmap_melted, n_top_highlight = NULL, cell_types = N
                              ids_to_highlight = NULL, max.overlaps = 40,
                              min.segment.length = unit(0.001,'mm'),
                              label_alpha = 0.8,
-                             label_size = 3) {
+                             label_size = 3,
+                             tas_color = 'blue',
+                             tas_linetype = 'dashed') {
   
   if(!requireNamespace("cmapR")) {
     stop('Please load cmapR to continue')
@@ -32,7 +37,9 @@ cmap2_cc_ss_plot <- function(cmap_melted, n_top_highlight = NULL, cell_types = N
   if(!requireNamespace("tidyverse")) {
     stop('Please load tidyverse to continue')
   }
-  
+  if(!requireNamespace("purrr")) {
+    stop('Please load purrr to continue')
+  }
 #tas_function <- function(x,y) {sqrt(x*y/978)}
 
 #annotating signatures
@@ -55,6 +62,16 @@ ids_to_highlight <- df2 %>%
   pull(id)
 } 
 
+geom_tas <- map(
+  seq(0.2, 0.8, 0.2), 
+  ~ geom_function(fun = function(x) (978 * .x^2) / x, 
+                  color = tas_color, 
+                  linetype = tas_linetype
+  )
+)
+
+
+
 
 
 g <- ggplot(df2, aes(x = cc_q75,y = ss_ngene, color = Signature)) + 
@@ -69,10 +86,17 @@ g <- ggplot(df2, aes(x = cc_q75,y = ss_ngene, color = Signature)) +
   geom_rect(xmin = 0.2, xmax = 1, ymin = 200, ymax = 1000,fill = 'lemonchiffon', 
             color = 'grey80', alpha = 0.4) + 
   geom_point() + 
+  geom_tas + 
   labs(y = 'Signature strength',
        x = 'Replicate correlation') + 
   scale_color_manual(values = c('thistle3','cadetblue3','goldenrod3','darkolivegreen3')) + 
+  scale_y_continuous(
+    limits = c(0, 1000), 
+    sec.axis = sec_axis(~ sqrt(. / 978), "TAS", 
+                        breaks = seq(0.2, 0.8, 0.2))) + 
   theme_minimal()
+
+
 
 if(!is.null(ids_to_highlight)) {
   if(!requireNamespace("ggrepel")) {
